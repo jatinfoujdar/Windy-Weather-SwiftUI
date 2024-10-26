cc0c45a5a8446be81abde742eac171271f301426
Jatin Foujdar <jatin321001@gmail.com>
2024-10-26 11:51:59 +0530

    Initial Commit

---
+++ Windy-Weather-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Windy-Weather-SwiftUI
+//
+//  Created by jatin foujdar on 26/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Windy-Weather-SwiftUI/Windy_Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Windy_Weather_SwiftUIApp.swift
+//  Windy-Weather-SwiftUI
+//
+//  Created by jatin foujdar on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Windy_Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
