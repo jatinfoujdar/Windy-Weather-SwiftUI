d4836019e3d1a1ac389b8e04d488bd4bca78f97e
Jatin Foujdar <jatin321001@gmail.com>
2024-10-26 14:09:50 +0530

    sample data commit

--- Windy-Weather-SwiftUI/ContentView.swift
+++ Windy-Weather-SwiftUI/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("32 °C")
+                .font(.largeTitle)
+                .foregroundStyle(.blue)
+            
+            Spacer()
         }
         .padding()
     }
