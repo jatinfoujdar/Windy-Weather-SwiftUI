faeb2b141911ca2e5d4ce5da21397abfe3de1f61
Jatin Foujdar <jatin321001@gmail.com>
2024-10-26 16:01:05 +0530

    list detail commit

---
+++ Windy-Weather-SwiftUI/View/ListDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ListDetail.swift
+//  Windy-Weather-SwiftUI
+//
+//  Created by jatin foujdar on 26/10/24.
+//
+
+import SwiftUI
+
+struct ListDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListDetail()
+}
